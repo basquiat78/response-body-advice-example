84c2d81b08734dc5aaa3d171d755a206dad84a20
jeanbasquiat <basquiat.yoon@rigolabs.io>
2024-08-22 16:34:19 +0900

    Initial commit

--- src/main/kotlin/io/basquiat/response/global/utils/PaginationUtil.kt
+++ src/main/kotlin/io/basquiat/response/global/utils/PaginationUtil.kt
@@ -1,21 +1,21 @@
 package io.basquiat.response.global.utils
 
 import io.basquiat.response.global.model.response.Pagination
-import io.basquiat.response.global.type.EntityLongIdentifiable
+import io.basquiat.response.global.type.LongIdentifiable
 
 fun <T> isLast(data: List<T>, size: Long): Boolean {
     val dataSize = data.size
     return dataSize <= size.toInt()
 }
 
-fun <T: EntityLongIdentifiable> getNextId(data: List<T>): Long? {
+fun <T: LongIdentifiable> getNextId(data: List<T>): Long? {
     if(data.isEmpty()) {
         return null
     }
     return data.last().id
 }
 
-fun <T: EntityLongIdentifiable> make(data: List<T>, size: Long): Pair<List<T>, Pagination> {
+fun <T: LongIdentifiable> make(data: List<T>, size: Long): Pair<List<T>, Pagination> {
     val isLast = isLast(data, size)
     // 마지막 페이지라면 조회한 리스트를 그대로
     val result = if(isLast) data else data.dropLast(1)
